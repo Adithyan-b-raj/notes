797e012174ba24cff36cd5411e66b5dec096561d
adithyan <145749590+Adithyan-b-raj@users.noreply.github.com>
2025-03-19 08:21:46 +0530

    Update FIFOpage.c

--- FIFOpage.c
+++ FIFOpage.c
@@ -1,39 +1,26 @@
 #include<stdio.h>
-
 void main() { 
-    int i, j, n, rs[50], frame[10], nf, k, avail, pagefault = 0, pagehit = 0; 
-
+    int i, j, n, rs[50], frame[10], nf, k, avail, pagefault = 0, pagehit = 0;
     printf("\nEnter the number of pages:\n"); 
     scanf("%d", &n); 
-
     printf("\nEnter the reference string:\n"); 
     for(i = 0; i < n; i++) 
         scanf("%d", &rs[i]); 
-
     printf("\nEnter the number of frames: "); 
     scanf("%d", &nf); 
-
     for(i = 0; i < nf; i++) 
         frame[i] = -1; 
-
     j = 0; 
     printf("Ref string\tPage frames\t\tPage Fault/Page Hit\n"); 
     for(i = 0; i < n; i++) { 
-        printf("%d\t\t", rs[i]); 
-
-        // Display the current page frames first
+        printf("%d\t\t", rs[i]);
         for(k = 0; k < nf; k++) 
             printf("%d\t", frame[k]); 
         printf("\t");
-
         avail = 0;
-        
-        // Check if the page is already in one of the frames (page hit)
         for(k = 0; k < nf; k++) 
             if(frame[k] == rs[i]) 
                 avail = 1; 
-
-        // If the page is not available (page fault)
         if(avail == 0) { 
             frame[j] = rs[i]; 
             j = (j + 1) % nf; 
